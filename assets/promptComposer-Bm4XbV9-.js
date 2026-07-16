2075b4d1b7d4f42b45bff9d3410ee2196b9a8baa
abhibelle2-ai <abhi.belle2@gmail.com>
2026-07-16 13:55:14 +0530

    Rebuild static Pages bundle for Gear 2 design pass

--- assets/promptComposer-Bm4XbV9-.js
+++ assets/promptComposer-Bm4XbV9-.js
@@ -1,4 +1,4 @@
-import{_ as e,f as t,u as n}from"./static-index-ClarH2WH.js";var r=`SIDE VIEW PROMPT\r
+import{_ as e,f as t,u as n}from"./static-index-DWPs-BMI.js";var r=`SIDE VIEW PROMPT\r
 \r
 \r
 ═══════════════════════════════════════════════════════\r
